d8ce108e9cb26bae66388e087cd31d54cbd6da99
AaronJi <jiluoaaron@hotmail.com>
2023-05-10 00:07:13 +0800

    add0510

--- src/main/java/search/searcher.java
+++ src/main/java/search/searcher.java
@@ -1,5 +1,7 @@
 package search;
 
+import java.util.List;
+import java.util.ArrayList;
 public class searcher {
 
     static int binarySearch(int[] array, int key) {
@@ -22,5 +24,176 @@ public class searcher {
 
         return -1;
     }
+
+    /*
+     * 33 Medium
+     * 搜索旋转排序数组
+     * 时间复杂度：O(logn)
+     * 空间复杂度：O(1)
+     * */
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /*
+     * 29 Medium
+     * 两数相除
+     * 时间复杂度：O(log^2C)，其中 C 表示 32 位整数的范围
+     * 空间复杂度：O(1)
+     * */
+    public int divide(int dividend, int divisor) {
+        // 考虑被除数为最小值的情况
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            }
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        // 考虑除数为最小值的情况
+        if (divisor == Integer.MIN_VALUE) {
+            return dividend == Integer.MIN_VALUE ? 1 : 0;
+        }
+        // 考虑被除数为 0 的情况
+        if (dividend == 0) {
+            return 0;
+        }
+
+        // 一般情况，使用二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        boolean rev = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            rev = !rev;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            rev = !rev;
+        }
+
+        int left = 1, right = Integer.MAX_VALUE, ans = 0;
+        while (left <= right) {
+            // 注意溢出，并且不能使用除法
+            int mid = left + ((right - left) >> 1);
+            boolean check = quickAdd(divisor, mid, dividend);
+            if (check) {
+                ans = mid;
+                // 注意溢出
+                if (mid == Integer.MAX_VALUE) {
+                    break;
+                }
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return rev ? -ans : ans;
+    }
+
+    // 快速乘
+    public boolean quickAdd(int y, int z, int x) {
+        // x 和 y 是负数，z 是正数
+        // 需要判断 z * y >= x 是否成立
+        int result = 0, add = y;
+        while (z != 0) {
+            if ((z & 1) != 0) {
+                // 需要保证 result + add >= x
+                if (result < x - add) {
+                    return false;
+                }
+                result += add;
+            }
+            if (z != 1) {
+                // 需要保证 add + add >= x
+                if (add < x - add) {
+                    return false;
+                }
+                add += add;
+            }
+            // 不能使用除法
+            z >>= 1;
+        }
+        return true;
+    }
+
+    public int divideSemiBinSearch(int dividend, int divisor) {
+        // 考虑被除数为最小值的情况
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            }
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        // 考虑除数为最小值的情况
+        if (divisor == Integer.MIN_VALUE) {
+            return dividend == Integer.MIN_VALUE ? 1 : 0;
+        }
+        // 考虑被除数为 0 的情况
+        if (dividend == 0) {
+            return 0;
+        }
+
+        // 一般情况，使用类二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        boolean rev = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            rev = !rev;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            rev = !rev;
+        }
+
+        List<Integer> candidates = new ArrayList<Integer>();
+        candidates.add(divisor);
+        int index = 0;
+        // 注意溢出
+        while (candidates.get(index) >= dividend - candidates.get(index)) {
+            candidates.add(candidates.get(index) + candidates.get(index));
+            ++index;
+        }
+        int ans = 0;
+        for (int i = candidates.size() - 1; i >= 0; --i) {
+            if (candidates.get(i) >= dividend) {
+                ans += 1 << i;
+                dividend -= candidates.get(i);
+            }
+        }
+
+        return rev ? -ans : ans;
+    }
+
 
 }
--- src/main/java/utils/other.java
+++ src/main/java/utils/other.java
@@ -655,4 +655,66 @@ public class other {
         return slow;
     }
 
+    /*
+     * 27 Medium
+     *  找出字符串中第一个匹配项的下标
+     * 给你两个字符串haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。如果needle 不是 haystack 的一部分，则返回 -1 。
+     * 时间复杂度：O(n*m)
+     * 空间复杂度：O(1)
+     * */
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /*
+     * 28 Medium
+     *  找出字符串中第一个匹配项的下标
+     * 给你两个字符串haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。如果needle 不是 haystack 的一部分，则返回 -1 。
+     * 时间复杂度：O(n+m)
+     * 空间复杂度：O(m)
+     * */
+    public int strStrKMP(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        if (m == 0) {
+            return 0;
+        }
+        int[] pi = new int[m];
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (needle.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            pi[i] = j;
+        }
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == m) {
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+
+
+
 }
