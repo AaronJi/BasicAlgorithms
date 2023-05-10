fde6b8efe7522a405c6f34238158529e454648fb
AaronJi <jiluoaaron@hotmail.com>
2023-05-10 22:02:36 +0800

    add0512

--- src/main/java/search/searcher.java
+++ src/main/java/search/searcher.java
@@ -41,6 +41,28 @@ public class searcher {
     }
 
     /*
+     * 35 Easy
+     * 搜索插入位置
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     * 时间复杂度：O(logn)
+     * 空间复杂度：O(1)
+     * */
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = n;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    /*
      * 34 Medium
      * 在排序数组中查找元素的第一个和最后一个位置
      * 时间复杂度：O(logn)
