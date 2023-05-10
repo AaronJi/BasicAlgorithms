3686a3e27e37afef05e37af05c694ce6e7065a57
AaronJi <jiluoaaron@hotmail.com>
2023-05-10 21:50:20 +0800

    add0511

--- src/main/java/search/searcher.java
+++ src/main/java/search/searcher.java
@@ -25,6 +25,36 @@ public class searcher {
         return -1;
     }
 
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    /*
+     * 34 Medium
+     * 在排序数组中查找元素的第一个和最后一个位置
+     * 时间复杂度：O(logn)
+     * 空间复杂度：O(1)
+     * */
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
     /*
      * 33 Medium
      * 搜索旋转排序数组
